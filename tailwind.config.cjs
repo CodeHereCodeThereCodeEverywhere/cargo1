01a0e8757e4955df7fdf38187ec6fbad72044bbc
Panayot Petkov <panayotsky.dev@gmail.com>
2023-09-11 00:17:57 +0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,3 +21,17 @@ module.exports = {
 	},
 	plugins: [],
 }
+
+
+// screens: {
+// 	'phone': {'min': '300px', 'max': '640px'},
+// 	sm:{'min': '640px', 'max': '768px'},
+// 	md:{'min': '768px', 'max': '1280px'},
+// 	mdx:{'min':'922px','max':'1280px'} ,
+// 	xl:"1280px",
+// 	xxl:"1536px",
+// 	zxl: "1919px",
+// 	smd: "680px",
+// 	xs:"320xp",		
+// 	ssmm:  {'min': '640px', 'max': '1224px'},
+//   },
